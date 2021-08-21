bd8216285ae06f82456bb7ff947c666c71689454
Xavier Arias <xavier_rate@outlook.com>
2021-08-21 11:55:40 +1000

    Initial commit

---
+++ Assets/RW/Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float speed = 1;
+    private float maxY = -5;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * speed);
+        if (transform.position.y < maxY)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "ShipModel")
+        {
+            Game.GameOver();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/RW/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private Spawner spawner;
+
+	void Update ()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * 5);
+        if (transform.position.y > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Asteroid>() != null)
+        {
+            Game.AsteroidDestroyed();
+            Destroy(gameObject);
+            spawner.asteroids.Remove(collision.gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
